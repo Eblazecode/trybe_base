7c22fa452314fb78f2f32e317a1a1d40b9501186
daniel enoch <dynablaze@gmail.com>
2024-10-30 07:00:44 +0100

    relaunching v.1 , deployed application to prod. and changes to allowed host

--- NETRYBE_BASE/settings.py
+++ NETRYBE_BASE/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -25,8 +24,7 @@ SECRET_KEY = 'django-insecure-)gspu1!@q-qu_)ucc_gp^_gcj42=2dt)#u=sldun##7srq^#*_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["trybe-base.vercel.app", "localhost", "127.0.0.1"]
-
+ALLOWED_HOSTS = ["netrybe.ng", "www.netrybe.ng", "trybe-base.vercel.app", "localhost", "127.0.0.1"]
 
 # Application definition
 
@@ -43,7 +41,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',           # added for deploying to vercel
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # added for deploying to vercel
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -72,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'NETRYBE_BASE.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -82,7 +79,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -102,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -114,12 +109,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # added for deploying to vercel
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # added for deploying to vercel
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
